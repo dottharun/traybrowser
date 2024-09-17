0456995c60b896f7e75164d1e320223ea9546a84
Tharun T <tharun242424@gmail.com>
2024-09-17 16:49:24 +0530

    fix: text cleanup

--- src/main.c
+++ src/main.c
@@ -6,30 +6,28 @@
 
 int main() {
     // opening html file
-    const char* path         = "./res/cern.html";
+    // must be freed after use
+    const char* path         = "./res/theproject.html";
     char*       file_content = read_file_to_string(path);
 
+    // rl window init
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_ALWAYS_RUN);
-
     const int factor = 80;
     InitWindow(factor * 16, factor * 9, "TRAYBROWSER");
+    SetTargetFPS(30);
 
     // font init
-    Font scp_font = LoadFontEx(
-        "./res/ComicShannsMono/ComicShannsMonoNerdFont-Regular.otf",
-        96,
-        0,
-        0
-    );
+    const char* font_path =
+        "./res/ComicShannsMono/ComicShannsMonoNerdFont-Regular.otf";
+    Font    scp_font     = LoadFontEx(font_path, 96, 0, 0);
     Vector2 fontPosition = {100.0f, 20.0f};
-
-    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(DARKGRAY);
 
-        DrawText("website", 500, 5, 30, WHITE);
+        // draw something
+        DrawText("website:", 500, 5, 30, WHITE);
         DrawTextEx(scp_font, file_content, fontPosition, 28, 1, WHITE);
 
         EndDrawing();
@@ -37,11 +35,6 @@ int main() {
 
     CloseWindow();
 
-    // freeing file_content
-    if (file_content) {
-        printf("File content:\n%s\n", file_content);
-        free(file_content); // Remember to free the allocated memory
-    }
-
+    free(file_content);
     return 0;
 }
